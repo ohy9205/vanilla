ededa0a5a53e9c3d3a804f2bbbd87e917ae6e495
yeon <ohy9205@naver.com>
2024-06-20 17:49:14 +0900

    타이머 구현

--- scroll-timer/index.js
+++ scroll-timer/index.js
@@ -1,9 +1,23 @@
 {
+  const COUNT_TIME = 3;
+  const NOTIFICATION_TEXT = {
+    active: `🕖 ${COUNT_TIME}초 구경하면 보상을 받아요`, // default값
+    pause: "⬇️ 스크롤해야 시간이 줄어요",
+    done: "🎉 리워드가 지급되었어요",
+  };
+
   document.addEventListener("DOMContentLoaded", () => {
-    const $main = document.getElementsByTagName(".container")[0];
+    const $main = document.querySelector(".container");
     const $list = document.querySelector(".list");
     const $end = document.querySelector(".end");
+    const $notification = document.querySelector(".notification");
+
+    $notification.textContent = NOTIFICATION_TEXT.active;
+    let notificationMode = undefined;
+
     let $listLastItem = $list.lastElementChild;
+
+    let updatedTime = COUNT_TIME;
 
     // 1. 무한스크롤 (5개씩)
     // 관찰자 설정
@@ -11,18 +25,15 @@
       entries, // IntersectionObserverEntry 인스턴스 배열
       observer // 콜백 실행되는 해당 인스턴스
     ) => {
-      // 무한스크롤 종료
-      if ($list.childElementCount >= 20) {
-        observer.disconnect();
-        return;
-      }
       // 관찰요소가 교차되는지 판별
       if (entries[0].isIntersecting) {
         // 데이터 추가
         for (let i = 0; i < 5; i++) {
-          const $newListItem = makeNewListItem(
-            Number($listLastItem.textContent) + i + 1
-          );
+          const curIndex = Number($listLastItem.textContent) + i + 1;
+          const $newListItem = makeElement("li", "list-item");
+          $newListItem.setAttribute("data-index", curIndex);
+          $newListItem.textContent = curIndex;
+
           $list.append($newListItem);
         }
         $listLastItem = $list.lastElementChild;
@@ -40,13 +51,67 @@
     );
 
     observer.observe($end);
+
+    // 2. 스크롤 시 타이머
+    // type1. 스크롤중
+    const scrollCallback = () => {
+      if (notificationMode === "done") {
+        return;
+      }
+      notificationMode = "active";
+      $notification.textContent = NOTIFICATION_TEXT.active;
+    };
+
+    // type2. 스크롤멈춤
+    const scrollendCallback = () => {
+      if (notificationMode === "done") {
+        return;
+      }
+      notificationMode = "pause";
+      $notification.textContent = NOTIFICATION_TEXT.pause;
+    };
+
+    // type3. 타이머완료
+    const timerCompleted = () => {
+      $notification.textContent = NOTIFICATION_TEXT.done;
+      $notification.classList.add("done");
+      $main.removeEventListener("scroll", scrollCallback);
+      $main.removeEventListener("scrollend", scrollendCallback);
+    };
+
+    $main.addEventListener("scroll", scrollCallback);
+    $main.addEventListener("scrollend", scrollendCallback);
+
+    let curCount = 0;
+    const timer = setInterval(() => {
+      if (++curCount == 2 && notificationMode === undefined) {
+        $notification.textContent = NOTIFICATION_TEXT.pause;
+      } else if (notificationMode === "active") {
+        NOTIFICATION_TEXT.active = NOTIFICATION_TEXT.active.replace(
+          updatedTime,
+          --updatedTime
+        );
+        if (updatedTime <= 0) {
+          timerCompleted();
+          clearInterval(timer);
+        }
+      }
+    }, 1000);
   });
 }
 
-const makeNewListItem = (index) => {
-  const $listItem = document.createElement("li");
-  $listItem.classList.add(`list-item`);
-  $listItem.setAttribute("data-index", `${index}`);
-  $listItem.textContent = index;
-  return $listItem;
+const makeElement = (tag, classList) => {
+  const $elem = document.createElement(tag);
+  if (Array.isArray(classList)) {
+    $elem.classList.add(...classList);
+  } else {
+    $elem.classList.add(classList);
+  }
+  return $elem;
+};
+
+const startTimer = (timer, callback) => {
+  return setInterval(() => {
+    callback();
+  }, timer);
 };
